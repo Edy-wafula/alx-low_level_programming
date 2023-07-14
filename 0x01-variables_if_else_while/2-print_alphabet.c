a919c7e4967999ab3cef5dd14c4e58a15f8a9abc
Edy-wafula <edyrenzprince@gmail.com>
2023-07-14 12:24:07 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	purchar('\n');
+	return (0);
+}
