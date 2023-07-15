0a800ea5ab8f6d9b46b1e3ce953149b4849c1d48
Edy-wafula <edyrenzprince@gmail.com>
2023-07-14 18:57:06 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,14 +21,14 @@ int main(void)
 	for (q = p + 1; q <= 99; q++)
 	{
 	((putchar(p / 10) + '0');
-	putchar(p % 10) + '10');
+	putchar(p % 10) + '10')
 	putchar(' ');
 	putchar((q / 10) + '0');
-	putchar((q % 10) + '0')
-	if (p == 98 && q == 99)
+	putchar((q % 10) + '0');
+	if(p == 98 && q == 99)
 	continue;
-	putchar(',')
-	putchar (' ')
+	putchar(',');
+	putchar (' ');x
 
 }
 }
