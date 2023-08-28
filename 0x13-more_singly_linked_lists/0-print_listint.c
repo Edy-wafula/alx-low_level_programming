b27b44f543d3ff81a61f68c4fa87460aaaa84a73
Edy-wafula <edyrenzprince@gmail.com>
2023-08-28 06:49:11 -0700

    my header file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * size_t print_listint - prints all the elements of a listint_t list.
+ * @h: linked list of type type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t kim = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		kim++;
+		h = h->next;
+	}
+
+	return (kim);
+}
