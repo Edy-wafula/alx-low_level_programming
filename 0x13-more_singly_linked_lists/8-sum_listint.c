b59126e133f0c0fd32671926ee90349241dc9570
Edy-wafula <edyrenzprince@gmail.com>
2023-08-29 05:36:08 -0700

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,25 +1,20 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * sum_listint - returns the sum of all the data in a listint_t linked list.
  * @head: pointer to the head of the linked list
- * @index: index of the node that starts at 0
  *
- * Return: pointer to the node we are looking for, or NULL not exist
+ * Return: result sum
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int sum_listint(listint_t *head)
 {
 	int f = 0;
-	listint *vay = head;
 
 	while (head != NULL)
 	{
-		if (f == index)
-			return (head);
-
-		 head = head->next;
-		f++;
+		f += head->n;
+		head = head->next;
 	}
 
-	return (NULL);
+	return (f);
 }
