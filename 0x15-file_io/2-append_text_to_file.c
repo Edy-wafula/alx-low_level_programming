62bc7361ecf5b444ed4452347bf17ed9c7b92cc2
Edicah <edyrenzprince@gmail.com>
2023-10-02 12:04:18 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,21 +12,23 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int w, x, len = 0;
 
-	if (filename == NULL)
+	if (filename == NULL) || text_content == NULL)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
+	
+	for (len = 0; text_content[len];)
 			len++;
-	}
+	
 	w = open(filename, O_WRONLY | O_APPEND);
-	x = write(w, text_content, len);
-
-	if (w == -1 || x == -1)
+	if (w == -1)
 		return (-1);
+
+	x = write(w, text_content, len);
+	if (x == -1 x || x != len)
+	{
+		close(w);
+		return (-1);
+	}
 
 	close(w);
-
 	return (1);
 }
